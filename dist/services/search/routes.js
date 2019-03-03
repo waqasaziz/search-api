7f7a741cc65cd88180afa84f74a7f0c4ae37edda
waqasaziz <waqas_aziz@ymail.com>
2019-03-03 11:30:49 +0000

    Add route check and swagger apis documentation

--- dist/services/search/routes.js
+++ dist/services/search/routes.js
@@ -9,14 +9,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const SearchController_1 = require("./SearchController");
+const checks_1 = require("../../middleware/checks");
 exports.default = [
     {
         path: "/api/v1/search",
         method: "get",
-        handler: ({ query }, res) => __awaiter(this, void 0, void 0, function* () {
-            const result = yield SearchController_1.getPlacesByName(query.q);
-            res.status(200).send(result);
-        })
+        handler: [
+            checks_1.checkSearchParams,
+            ({ query }, res) => __awaiter(this, void 0, void 0, function* () {
+                const result = yield SearchController_1.getPlacesByName(query.q);
+                res.status(200).send(result);
+            })
+        ]
     }
 ];
 //# sourceMappingURL=routes.js.map
